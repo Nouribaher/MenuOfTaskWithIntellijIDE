b92d66f9fab79961bf384acbe04e7d2307e6c499
Nouri Baher <80018839+Nouribaher@users.noreply.github.com>
2021-03-19 22:23:45 +0100

    Writing comments for the Main Of TodoList Test  class

--- TodoList/MainOfTodoListTest.java
+++ TodoList/MainOfTodoListTest.java
@@ -1,3 +1,13 @@
+/* The unit Testing of the TodoList compiles an app that simply updates "Menue of the Task !"
+This will allow the user to track compiler errors for TODO list classes and mothds
+to start solving problems according to error combinations (syntax errors and runtime errors).
+*@author  Nouri Baher
+*@Softwear Intellij IDEA
+*@version 2020.3
+*author name-text:org.junit.jupiter:junit-jupiter:5.4.2
+* @since  Mon, Feb 22, 2021 till Fri, Mar 19, 2021
+ */
+
 package TodoList;
 
 import java.io.ByteArrayInputStream;
@@ -13,10 +23,20 @@ public class MainOfTodoListTest {
     MainOfTodoListTest(){
 
     }
+    /*
+    Junit Assert is a method useful in determining Pass or Fail status of a test case, where the assert methods are provided
+    by the class org.junit.Assert which extends java.lang.Object class.
+     */
     @Test
     void addsTwoNumbers() {
         Assertions.assertEquals(2, 2, "1 + 1 should equal 2");
     }
+    
+    /*
+     @Test Assertions.assertEquals method  provides a detailed summary of the Task test execution. This is helpful in tracking
+    the quality of the pipeline, as well as for troubleshooting failures.
+     */
+    
     @Test
     void tasktest(){
         Task task1 = new Task("nour","1978-10-03","ITC","DONE");
@@ -28,6 +48,10 @@ public class MainOfTodoListTest {
         System.out.println(actualTask);
         Assertions.assertEquals(actualTask,task2,"The test of task is fail");
     }
+    /*
+    @Test Assertions.assertEquals method  provides a detailed summary of the uppdate of task execution. This is helpful in tracking
+    the quality of the pipeline, as well as for troubleshooting failures.
+    */
     @Test
     void uppdateoftask(){
         Task task1 = new Task("nour","1978-10-03","ITC","DONE");
@@ -41,6 +65,12 @@ public class MainOfTodoListTest {
         System.out.println(actualTask);
         Assertions.assertEquals(actualTask,task1,"The test of task is fail");
     }
+    
+    /*
+    @Test Assertions.assertEquals method  provides a detailed summary of the remove the task execution. This is helpful in tracking
+    the quality of the pipeline, as well as for troubleshooting failures.
+     */
+    
     @Test
     void removeTask() {
         Task task1 = new Task("nour","1978-10-03","ITC","DONE");
@@ -55,8 +85,10 @@ public class MainOfTodoListTest {
         Assertions.assertEquals(actualTask,task2,"The test of task is fail");
     }
 
-
-    private void provideInput(String data) {
+       /*How to switch System.in? please go the git rip and Please read
+       issue # 1 and add some comments that will help us develop the code
+      */
+       private void provideInput(String data) {
         ByteArrayInputStream testIn;
         testIn = new ByteArrayInputStream(data.getBytes());
         System.setIn(testIn);
